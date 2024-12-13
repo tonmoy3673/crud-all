8d96bbc156091b2ac4c921a317c81fb917587128
tonmoy <tonmoy3673@gmail.com>
2024-12-13 17:19:43 +0600

    :bulb: implemented submit form button

--- src/Components/Pages/AddProducts.jsx
+++ src/Components/Pages/AddProducts.jsx
@@ -1,15 +1,28 @@
 
 import userImage from "../../assets/Images/user.jpg";
 const AddProducts =()=>{
+    const handleForm=(e)=>{
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const age = form.age.value;
+        const details = form.details.value;
+
+        const user ={
+            name,email,age,details
+        }
+        console.log(user);
+    }
 
     return(
-        <div className="grid grid-cols-1 md:grid-cols-5 items-center justify-center py-6">
+        <div className="grid grid-cols-1 md:grid-cols-5 items-center justify-center py-6 gap-y-5 md:gap-y-0">
             <div className="md:col-span-3">
                 <img src={userImage} className="rounded-lg"/>
             </div>
             
             <div className="md:col-span-2 -ms-0 md:-ms-20">
-                <form className="bg-slate-600 py-4 md:py-8 px-3 md:px-6 rounded-xl">
+                <form onSubmit={handleForm} className="bg-slate-600 py-4 md:py-8 px-3 md:px-6 rounded-xl">
                 <h2 className="mb-6 text-center text-xl text-white">Add Product Here</h2>
                     <div className="text-white text-center mb-5 pt-3">
                         <label htmlFor="name">Name : </label>
@@ -17,11 +30,15 @@ const AddProducts =()=>{
                     </div>
                     <div className="text-white text-center mb-5">
                         <label htmlFor="email">Email : </label>
-                        <input id="email" className="px-2 rounded-md" type="email" placeholder="Enter Email"/>
+                        <input id="email" className="text-black px-2 rounded-md" type="email" placeholder="Enter Email"/>
                     </div>
                     <div className="text-white text-center mb-5">
                         <label htmlFor="age">Age : </label>
-                        <input id="age" className="px-2 rounded-md" type="text" placeholder="Enter Age"/>
+                        <input id="age" className="text-black px-2 rounded-md" type="text" placeholder="Enter Age"/>
+                    </div>
+                    <div className="text-white text-center flex items-center mb-5 justify-center gap-3">
+                        <label htmlFor="details">Details : </label>
+                        <textarea id="details" className="text-black px-2 rounded-md w-36" type="text" placeholder="Write Description"/>
                     </div>
                     <div className="text-white text-center py-3 mt-3">
                         <button type="submit" className="bg-teal-600 px-3 py-2 rounded-md text-sm">Submit</button>
--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -1,21 +1,21 @@
 import { Link, NavLink } from "react-router-dom";
 
 const Navbar=()=>{
+
+   
     return (
         <div className="py-5 md:py-8 flex items-center justify-between px-5 md:px-0 ">
             <div>
                 <Link to='/home' className="uppercase text-2xl text-teal-600 font-semibold ">All <span className="text-amber-500">Crud</span></Link>
             </div>
-            <nav className="flex gap-x-8 mx-auto">
+            <nav className="flex gap-x-4 md:gap-x-8 mx-auto">
                 <NavLink className="text-teal-600 font-semibold hover:text-amber-500 hover:scale-110 transition-all duration-400">
                     Home
                 </NavLink>
                 <NavLink className="text-teal-600 font-semibold hover:text-amber-500 hover:scale-110 transition-all duration-400">
                     About
                 </NavLink>
-                <NavLink className="text-teal-600 font-semibold hover:text-amber-500 hover:scale-110 transition-all duration-400">
-                    Contact
-                </NavLink>
+                
                 <NavLink className="text-teal-600 font-semibold hover:text-amber-500 hover:scale-110 transition-all duration-400">
                     Feedback
                 </NavLink>
