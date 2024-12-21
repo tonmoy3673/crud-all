f7efd9a25d4df583d4a59e2b45127054667fb287
tonmoy <tonmoy3673@gmail.com>
2024-12-21 10:55:50 +0600

    :tada: added details button

--- src/Components/Pages/User.jsx
+++ src/Components/Pages/User.jsx
@@ -8,6 +8,9 @@ const User = ({user}) => {
             <h2>{name}</h2>
             <p>{email}</p>
             <p>{age}</p>
+            <div className="py-4 md:py-8 ">
+                <button className="bg-green-700 py-2 px-3 rounded-md text-sm text-white hover:scale-110 transition-transform duration-300 cursor-pointer">Details</button>
+            </div>
         </div>
     );
 };
