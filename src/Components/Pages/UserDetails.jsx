1917afc177cf2893e86e1450489bbc1c669ddd9e
tonmoy <tonmoy3673@gmail.com>
2024-12-21 11:40:08 +0600

    :tada: display user details

--- src/Components/Pages/UserDetails.jsx
+++ src/Components/Pages/UserDetails.jsx
@@ -2,11 +2,16 @@ import { useLoaderData } from "react-router-dom";
 
 const UserDetails =()=>{
     const user = useLoaderData();
+    const {_id,name,email,age,details}=user;
     console.log(user);
     return (
-        <div>
-            <h2>User Details</h2>
-
+        <div className="py-3 md:py-10 text-center">
+            <h2 className="py-4">User Details</h2>
+            <p>id : {_id}</p>
+            <h2>Name : {name}</h2>
+            <p>Email : {email}</p>
+            <p>Age : {age}</p>
+            <p>Details : {details}</p>
         </div>
     )
 };
